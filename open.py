5340e0e5f62741ea5a989737721dd5888655948b
gnudo <goran.lovric@gnudo.com>
2014-04-14 23:03:40 +0200

    minimal example for fetching root password with Tkinter and running a command as super user

--- open.py
+++ open.py
@@ -1,14 +1,16 @@
 import Tkinter as tk
+import subprocess
 
-class PasswordDialog(tk.Toplevel):
+class MainApplication(tk.Frame):
     def __init__(self, parent):
-        tk.Toplevel.__init__(self)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
+        self.button = tk.Button(self)
         self.entry = tk.Entry(self, show='*')
         self.entry.bind("<KeyRelease-Return>", self.StorePassEvent)
         self.entry.pack()
         self.button = tk.Button(self)
-        self.button["text"] = "Submit"
+        self.button["text"] = "Open"
         self.button["command"] = self.StorePass
         self.button.pack()
 
@@ -16,39 +18,21 @@ class PasswordDialog(tk.Toplevel):
         self.StorePass()
 
     def StorePass(self):
-        self.parent.password = self.entry.get()
+        self.password = self.entry.get()
         self.destroy()
-        print '1: Password was', self.parent.password
-
-class MainApplication(tk.Frame):
-    def __init__(self, root):
-        tk.Frame.__init__(self, root)
-        self.password = None
-        self.button = tk.Button(self)
-        self.button["text"] = "Password"
-        self.button["command"] = self.GetPassword
-        self.button.pack()
-
-    def GetPassword(self):
-        self.wait_window(PasswordDialog(self))
-        print '2: Password was', self.password
+        self.parent.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    appz = MainApplication(root)
+    appz.pack(side="top", fill="both", expand=True)
     root.mainloop()
-    
-# import subprocess
-# 
-# password = 'asdf'
-# #proc = subprocess.Popen(
-# #  ['sudo','-p','','-S','/etc/init.d/test','restart'],
-# #   stdin=subprocess.PIPE)
-# proc = subprocess.Popen(
-# #    ['sudo','-p','','-S','echo','restart'],
-#     ['su','-c','ls'],
-#     stdin=subprocess.PIPE)
-# 
-# proc.stdin.write(password+'\n')
-# proc.stdin.close()
-# proc.wait()
+   
+    proc = subprocess.Popen(
+                            ['su','-c','ls'],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    proc.stdin.write(appz.password+'\n')
+    proc.stdin.close()
+    proc.wait()
+    print proc.stdout.read()
