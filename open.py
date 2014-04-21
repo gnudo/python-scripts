7b5791626c6ea911a2721ed664d987478cf247ef
gnudo <goran.lovric@gnudo.com>
2014-04-22 00:24:38 +0200

    working GUI for starting and stopping sshd

--- open.py
+++ open.py
@@ -1,38 +1,84 @@
 import Tkinter as tk
+import tkMessageBox
 import subprocess
 
-class MainApplication(tk.Frame):
+class PasswordGUI(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.parent = parent
-        self.button = tk.Button(self)
+        self.button = tk.Button(self,text="Open",command=self.Open)
+        self.button2 = tk.Button(self,text="Close",command=self.Close)
         self.entry = tk.Entry(self, show='*')
-        self.entry.bind("<KeyRelease-Return>", self.StorePassEvent)
         self.entry.pack()
-        self.button = tk.Button(self)
-        self.button["text"] = "Open"
-        self.button["command"] = self.StorePass
         self.button.pack()
+        self.button2.pack()
+        
+        
+    def runAsRoot(self,command,close):
+        p = subprocess.Popen(
+                            ['su','-c',command],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        p.stdin.write(self.password+'\n')
+        if close:
+            p.stdin.close()
+            p.wait()
+        return p
 
-    def StorePassEvent(self, event):
-        self.StorePass()
 
-    def StorePass(self):
+    def Open(self):
+        '''
+        open ssh access and open firewall
+        '''
+        self.Authenticate()
+        
+        # open ssh access
+        self.runAsRoot('/sbin/service sshd start', True)
+        
+        # DESTROY window etc.
+        self.Destroy() 
+        
+        
+    def Close(self):
+        '''
+        close ssh access and close firewall
+        '''
+        self.Authenticate()
+        
+        # close ssh access
+        self.runAsRoot('/sbin/service sshd stop', True)
+        
+        # DESTROY window etc.
+        self.Destroy() 
+        
+        
+    def Authenticate(self):
+        '''
+        pseudo-method for checking whether su password is correct
+        '''
         self.password = self.entry.get()
+        proc = self.runAsRoot('ls /', False)
+        proc.stderr.read(10)  # read "password prompt"
+        out, err = proc.communicate(self.password+'\n')
+        if (len(out) > 0) & (len(err) == 0):
+            return
+        elif (len(out) == 0) & (len(err) > 0):
+            tkMessageBox.showinfo("Fail", "Wrong password!")
+            exit()
+        else:
+            tkMessageBox.showinfo("Fail", "Something else is wrong!")
+            exit()
+
+        
+    def Destroy(self):
+        '''
+        destroy windows etc.
+        '''
         self.destroy()
         self.parent.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
-    appz = MainApplication(root)
+    appz = PasswordGUI(root)
     appz.pack(side="top", fill="both", expand=True)
     root.mainloop()
-   
-    proc = subprocess.Popen(
-                            ['su','-c','ls'],
-                            stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    proc.stdin.write(appz.password+'\n')
-    proc.stdin.close()
-    proc.wait()
-    print proc.stdout.read()
